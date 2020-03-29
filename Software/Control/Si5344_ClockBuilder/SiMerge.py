df0ab7234890b87c7154d6a37198e2c2b2e3da21
Ivan Vasilev <ivan.v.vasilev@gmail.com>
2020-03-29 18:54:41 +0300

    Add a script to merge multiple Si534x configurations in a single file

---
+++ Software/Control/Si5344_ClockBuilder/SiMerge.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+import re
+import datetime
+
+from sys import argv
+from io import open
+
+def printUsage():
+    print("""
+A Python script to merge multiple Si534x regsiter congirurations into a single file
+
+Usage:
+  ./SiMerge.py SiRegOutFile.h SiRegInFile1.h SiRegInFile2.h [SiRegFile3.h] []
+""")
+
+
+def printHeader(outfile, infiles):
+    outfile.write("""
+/******************************************************************************
+ * Si534x Register Configuration header file
+ *
+""")
+    outfile.write(" * Inputs: " + infiles + "\n");
+    outfile.write(" *\n * Timestamp: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    outfile.write("""
+ *
+ *****************************************************************************/
+
+""")
+
+def extractRegType(infile, outfile):
+    content = infile.read()
+    typedef_regex = r"(?s)(typedef struct).*?(_register_t;)"
+    matches = re.finditer(typedef_regex, content, re.MULTILINE)
+
+    for matchNum, match in enumerate(matches, start=1):
+        # print ( match.group())
+        outfile.write(match.group())
+        outfile.write("\n\n")
+
+def extractPreamblePostamble(infile, outfile):
+    content = infile.read();
+    preamble_regex = r"(?s)(Start configuration preamble).*?(End configuration preamble)"
+    matches = re.finditer(preamble_regex, content, re.MULTILINE)
+
+    for matchNum, match in enumerate(matches, start=1):
+        # print ( match.group())
+        outfile.write("si5344_revd_register_t const si5344_preamble[] = {\n")
+        outfile.write("    /* " + match.group() + " */")
+        outfile.write("\n};\n\n")
+    
+    postamble_regex = r"(?s)(Start configuration postamble).*?(End configuration postamble)"
+    matches = re.finditer(postamble_regex, content, re.MULTILINE)
+
+    for matchNum, match in enumerate(matches, start=1):
+        # print ( match.group())
+        outfile.write("si5344_revd_register_t const si5344_postamble[] = {\n")
+        outfile.write("    /* " + match.group() + " */")
+        outfile.write("\n};\n\n") 
+
+def extractRegisters(infile, outfile, arrayName):
+    content = infile.read()
+    preamble_regex = r"(?s)(Start configuration registers).*?(End configuration registers)"
+    matches = re.finditer(preamble_regex, content, re.MULTILINE)
+
+    for matchNum, match in enumerate(matches, start=1):
+        # print ( match.group())
+        outfile.write("si5344_revd_register_t const " + arrayName + "[] = {\n")
+        outfile.write("    /* " + match.group() + " */")
+        outfile.write("\n};\n\n")
+    
+def printFooter(outfile):
+    
+
+def getArrayName(filename):
+    # Si5344-RevD-DeWille01-Registers_225792_SingleIn.h =>
+    # si5344_revd_dewille01_registers_225792_singlein
+    filename = filename.lower()
+    if filename.endswith(".h"):
+        filename = filename[:-4]
+    filename = filename.replace("-", "_")
+    return filename
+
+# It starts here
+if (len(argv) < 3):
+    printUsage()
+    quit()
+
+with open(argv[1], "w") as outfile:
+    print("Printing header...\n")
+    printHeader(outfile, ",".join(argv[2:]))
+   
+    # igore errors as input file contains non-ansi character(s)
+    with open(argv[2], errors="ignore") as infile:
+        print("Extracting register typedef...\n");
+        extractRegType(infile, outfile)
+
+    with open(argv[2], errors="ignore") as infile:
+        print("Extracting preamble and postamble...\n");
+        extractPreamblePostamble(infile, outfile)
+
+    extractedRegs = []
+    for i in range(2, len(argv)):
+        print("Extracting register configuration from " + argv[i] + "\n")
+        arrayName = getArrayName(argv[i])
+        extractedRegs.push(arrayName)
+        with open(argv[i], errors="ignore") as infile:
+            extractRegisters(infile, outfile, arrayName)
+
+    printFooter(outfile, extractedRegs)
+
+    print("Done")
+    
+
+
